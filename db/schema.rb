23ccf28e17ca771b8844c6c6fd15dc42c73fafcd
tuantea <tuan1722000@gmail.com>
2021-12-06 03:37:26 +0000

    add name age to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_06_012952) do
-     create_table "active_storage_attachments", force: :cascade do |t|
+ActiveRecord::Schema.define(version: 2021_12_06_030658) do
+
+  create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
     t.integer "record_id", null: false
@@ -47,8 +48,12 @@ ActiveRecord::Schema.define(version: 2021_12_06_012952) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.integer "age"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
 end
